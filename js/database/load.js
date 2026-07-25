fed3f170ffdad133b95634e5359ae4c187e8cbfb
hunghvt-cyber <thanhhunga4.wtf@gmail.com>
2026-07-25 23:18:26 +0700

    Update load.js

--- js/database/load.js
+++ js/database/load.js
@@ -1,1 +1,28 @@
-// message.js
+async function loadItems() {
+
+    const result =
+        await db
+            .from("items")
+            .select("*")
+            .order(
+                "id",
+                {
+                    ascending: false
+                }
+            );
+
+    if (result.error) {
+
+        showMessage(
+            "❌ " + result.error.message
+        );
+
+        return;
+
+    }
+
+    allItems = result.data;
+
+    renderItems();
+
+}
