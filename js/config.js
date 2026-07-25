596716af2b6ecd4270cfded62bf091d19090bf0d
hunghvt-cyber <thanhhunga4.wtf@gmail.com>
2026-07-25 23:16:57 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,11 @@
-// message.js
+const SUPABASE_URL =
+    "https://dypqawaqlrthhfxnmxom.supabase.co";
+
+const SUPABASE_KEY =
+    "sb_publishable_tZwvgJDIo89qG7sqp5oIJg_GoFa8qMG";
+
+const db =
+    supabase.createClient(
+        SUPABASE_URL,
+        SUPABASE_KEY
+    );
