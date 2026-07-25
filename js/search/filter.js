c7f2c41d0601d7ce808626b1dc96a48a9ac56ba7
hunghvt-cyber <thanhhunga4.wtf@gmail.com>
2026-07-25 23:27:40 +0700

    Update filter.js

--- js/search/filter.js
+++ js/search/filter.js
@@ -1,1 +1,31 @@
-v
+function filterItems(keyword) {
+
+    keyword =
+        keyword
+            .trim()
+            .toLowerCase();
+
+    if (keyword === "") {
+
+        return allItems;
+
+    }
+
+    return allItems.filter(item => {
+
+        const name =
+            (item.name || "")
+                .toLowerCase();
+
+        const location =
+            (item.location || "")
+                .toLowerCase();
+
+        return (
+            name.includes(keyword) ||
+            location.includes(keyword)
+        );
+
+    });
+
+}
