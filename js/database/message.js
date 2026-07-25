f12f386d84d8622eb45e25e24d861b77f2c40a33
hunghvt-cyber <thanhhunga4.wtf@gmail.com>
2026-07-25 23:17:24 +0700

    Update message.js

--- js/database/message.js
+++ js/database/message.js
@@ -1,1 +1,24 @@
-// message.js
+function showMessage(message) {
+
+    const box =
+        document.getElementById("message");
+
+    if (!box) {
+
+        return;
+
+    }
+
+    box.innerHTML = message;
+
+    box.classList.add("show");
+
+    setTimeout(() => {
+
+        box.classList.remove("show");
+
+        box.innerHTML = "";
+
+    }, 2500);
+
+}
