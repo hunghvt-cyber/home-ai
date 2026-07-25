f351efaab44e5ec8044c3904bf7c85cf6b5e9bb9
hunghvt-cyber <thanhhunga4.wtf@gmail.com>
2026-07-25 23:28:10 +0700

    Update search.js

--- js/search/search.js
+++ js/search/search.js
@@ -1,1 +1,31 @@
-// message.js
+function initSearch() {
+
+    const search =
+        document.getElementById(
+            "search"
+        );
+
+    if (!search) {
+
+        return;
+
+    }
+
+    search.addEventListener(
+
+        "input",
+
+        function() {
+
+            const items =
+                filterItems(
+                    search.value
+                );
+
+            renderItems(items);
+
+        }
+
+    );
+
+}
