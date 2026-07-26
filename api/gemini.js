1e73dd4f099d206570c9ace9943cfbabe479da66
hunghvt-cyber <thanhhunga4.wtf@gmail.com>
2026-07-27 00:33:40 +0700

    Update gemini.js

--- api/gemini.js
+++ api/gemini.js
@@ -38,7 +38,7 @@ export default async function handler(req, res) {
         const response =
             await fetch(
 
-                "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent?key=" +
+                "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=" +
 
                 process.env.GEMINI_API_KEY,
 
