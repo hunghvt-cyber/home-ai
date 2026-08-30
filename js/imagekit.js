e135f3bfbfb8b8df15b426dbb63140b955cfa314
hunghvt-cyber <thanhhunga4.wtf@gmail.com>
2026-08-30 23:40:52 +0700

    Add ImageKit configuration constants

---
+++ js/imagekit.js
@@ -0,0 +1,8 @@
+const IMAGEKIT_PUBLIC_KEY =
+    "public_dZe8G/hzBOxyyd0dwk85tSdyDmQ=";
+
+const IMAGEKIT_URL_ENDPOINT =
+    "https://ik.imagekit.io/hunghvt";
+
+const IMAGEKIT_AUTH_URL =
+    "https://home-ai-two-topaz.vercel.app/api/imagekit-auth";
